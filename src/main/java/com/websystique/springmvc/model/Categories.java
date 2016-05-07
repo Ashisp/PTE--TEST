c38705b6131ab60cb31f77a9899d0a63ece2884b
Nikesh Maharjan <nikeshmhr@gmail.com>
2016-05-08 00:22:48 +0545

    Regenerated model

--- src/main/java/com/websystique/springmvc/model/Categories.java
+++ src/main/java/com/websystique/springmvc/model/Categories.java
@@ -32,9 +32,8 @@ import javax.xml.bind.annotation.XmlTransient;
     @NamedQuery(name = "Categories.findAll", query = "SELECT c FROM Categories c"),
     @NamedQuery(name = "Categories.findByCatId", query = "SELECT c FROM Categories c WHERE c.catId = :catId"),
     @NamedQuery(name = "Categories.findByCatName", query = "SELECT c FROM Categories c WHERE c.catName = :catName"),
-    @NamedQuery(name = "Categories.findByCatTime", query = "SELECT c FROM Categories c WHERE c.catTime = :catTime")})
+    @NamedQuery(name = "Categories.findByTotalTime", query = "SELECT c FROM Categories c WHERE c.totalTime = :totalTime")})
 public class Categories implements Serializable {
-
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
@@ -48,8 +47,8 @@ public class Categories implements Serializable {
     private String catName;
     @Basic(optional = false)
     @NotNull
-    @Column(name = "cat_time", nullable = false)
-    private int catTime;
+    @Column(name = "total_time", nullable = false)
+    private int totalTime;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "catId")
     private Collection<Questions> questionsCollection;
 
@@ -60,10 +59,10 @@ public class Categories implements Serializable {
         this.catId = catId;
     }
 
-    public Categories(Integer catId, String catName, int catTime) {
+    public Categories(Integer catId, String catName, int totalTime) {
         this.catId = catId;
         this.catName = catName;
-        this.catTime = catTime;
+        this.totalTime = totalTime;
     }
 
     public Integer getCatId() {
@@ -82,12 +81,12 @@ public class Categories implements Serializable {
         this.catName = catName;
     }
 
-    public int getCatTime() {
-        return catTime;
+    public int getTotalTime() {
+        return totalTime;
     }
 
-    public void setCatTime(int catTime) {
-        this.catTime = catTime;
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
     }
 
     @XmlTransient
