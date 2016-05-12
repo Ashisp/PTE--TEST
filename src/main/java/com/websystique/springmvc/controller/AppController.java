c1a48cd58065b9aa371d40c134b0602e3e3631bc
Nikesh Maharjan <nikeshmhr@gmail.com>
2016-05-13 00:57:36 +0545

    RR-SAMC timer up and running

--- src/main/java/com/websystique/springmvc/controller/AppController.java
+++ src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -673,6 +673,10 @@ public class AppController {
         ans.setAnswer(choice);
         answersService.saveAnswers(ans);
         String s = req.getParameter("offset");
+        
+        String elapsedTime = req.getParameter("elapsedTime").toString();
+        req.getSession(false).setAttribute("startTime", elapsedTime);
+        
         int offset;
         if (s.isEmpty() || s.equals("")) {
             offset = 1;
